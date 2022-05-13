29272474368965db3950d6a5c107b839e4f8d5c5
keshiamukenge <mukenge.keshia@gmail.com>
2022-05-14 00:42:33 +0200

    feat: setup library base

---
+++ src/Experience/Renderer.js
@@ -0,0 +1,41 @@
+import * as THREE from 'three';
+
+export default class Renderer {
+  constructor(experience) {
+    this.experience = experience;
+    this.canvas = this.experience.canvas;
+    this.sizes = this.experience.sizes;
+    this.scene = this.experience.scene;
+    this.camera = this.experience.camera;
+
+    this.setInstance();
+    this.resize();
+    this.update();
+  }
+
+  setInstance() {
+    this.instance = new THREE.WebGLRenderer({
+      canvas: this.canvas,
+      antialias: true,
+    });
+
+    this.instance.physicallyCorrectLights = true;
+    this.instance.outputEncoding = THREE.sRGBEncoding;
+    this.instance.toneMapping = THREE.CineonToneMapping;
+    this.instance.toneMappingExposure = 1.75;
+    this.instance.shadowMap.enabled = true;
+    this.instance.shadowMap.type = THREE.PCFSoftShadowMap;
+    this.instance.setClearColor('#211d20');
+    this.instance.setSize(this.sizes.width, this.sizes.height);
+    this.instance.setPixelRatio(this.sizes.setPixelRatio);
+  }
+
+  resize() {
+    this.instance.setSize(this.sizes.width, this.sizes.height);
+    this.instance.setPixelRatio(this.sizes.setPixelRatio);
+  }
+
+  update() {
+    this.instance.render(this.scene, this.camera.instance);
+  }
+}
