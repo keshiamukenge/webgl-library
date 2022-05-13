05fc793179deab5447e8f87d37307feab36994c4
keshiamukenge <mukenge.keshia@gmail.com>
2022-05-13 20:27:13 +0200

    init

---
+++ src/app.js
@@ -0,0 +1,5 @@
+/* eslint-disable no-unused-vars */
+import SayHi from './class/SayHi';
+
+const name = 'Keshia';
+const example = new SayHi(name);
---
+++ src/class/SayHi.js
@@ -0,0 +1,5 @@
+export default class SayHi {
+  constructor(name) {
+    console.log(`Hello ${name} !`);
+  }
+}
